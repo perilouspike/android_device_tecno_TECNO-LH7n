90f0e7a408c63106fddd7452061998f5856939e1
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-12-05 18:23:06 +0530

    Trying to fix touch issue.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -192,7 +192,7 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TARGET_USES_MKE2FS := true
 TW_INCLUDE_FASTBOOTD := true
-TW_LOAD_VENDOR_MODULES := "focaltech_ts_fw_01.bin focaltech_ts_fw.bin novatek_ts_fw_144.bin novatek_ts_fw_jdi.bin novatek_ts_fw_tm.bin novatek_ts_fw.bin"
+TW_LOAD_VENDOR_MODULES := "ilitek_ts_fw.bin focaltech_ts_fw.bin novatek_ts_fw_144.bin novatek_ts_fw_jdi.bin novatek_ts_fw_tm.bin novatek_ts_fw.bin"
 
 # Crypto
 #TW_INCLUDE_CRYPTO := true
